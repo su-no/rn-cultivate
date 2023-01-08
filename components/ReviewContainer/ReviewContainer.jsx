4383ccee2fb8c7363421edaf9e3814d8b9eb993f
Hyojin <0910.shj@gmail.com>
2023-01-08 18:08:36 +0900

    Feat: 상세페이지 API get

--- components/ReviewContainer/ReviewContainer.jsx
+++ components/ReviewContainer/ReviewContainer.jsx
@@ -3,8 +3,9 @@ import Input from '../Input/Input';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import * as S from './styles';
 
-export default function ReviewContainer() {
+export default function ReviewContainer({ title }) {
   // TODO: firebase에서 Review 데이터를 받아와서 ReviewItem에 넘겨줌
+  // title로 검색
 
   return (
     <S.Container>
