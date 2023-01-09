0dd79a4950e7c1af551b4ea1422bdef3888eeca7
Hyojin <0910.shj@gmail.com>
2023-01-09 10:37:03 +0900

    feat: 리뷰 read
    .

--- common/utils.js
+++ common/utils.js
@@ -8,3 +8,14 @@ export const { width: screenWidth, height: screenHeight } =
 export const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 export const pwRegex =
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+// 2022-11-11 13:52 포맷으로 날짜를 받아오는 함수
+export const getDate = () => {
+  const date = new Date();
+  const year = String(date.getFullYear()).padStart(4, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minuites = String(date.getMinutes()).padStart(2, '0');
+  return `${year}-${month}-${day} ${hour}:${minuites}`;
+};
--- components/ReviewContainer/ReviewContainer.jsx
+++ components/ReviewContainer/ReviewContainer.jsx
@@ -1,32 +1,66 @@
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { authService, dbService } from '../../common/firebase';
+import { getDate } from '../../common/utils';
+import * as S from './styles';
 import Input from '../Input/Input';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import * as S from './styles';
 
 export default function ReviewContainer({ title }) {
-  // TODO: firebase에서 Review 데이터를 받아와서 ReviewItem에 넘겨줌
-  // title로 검색
+  // 리뷰 데이터
+  const [reviews, setRivews] = useState([]);
+
+  // firebase에서 title과 일치하는 리뷰 받아오는 함수
+  const getReviews = async () => {
+    const q = query(
+      collection(dbService, 'reviews'),
+      where('title', '==', title),
+    );
+
+    try {
+      const reviews = [];
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) =>
+        reviews.push({ id: doc.id, ...doc.data() }),
+      );
+      setRivews(reviews);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // firebase 현재 유저 정보
+  const user = authService.currentUser;
+  console.log(user);
+
+  const addReview = async (content, nickname) => {
+    const review = {
+      title,
+      content, // input에 입력한 내용
+      nickname, // firebase - currentUser displayName
+      date: new Date().toLocaleDateString(),
+    };
+  };
+
+  // 컴포넌트 마운트 시 리뷰 데이터 받아오기
+  useEffect(() => {
+    getReviews();
+  }, []);
 
   return (
     <S.Container>
       <View>
         <S.Title>후기 & 기대평</S.Title>
-        <Input />
-        <ReviewItem
-          content="너무 기대돼요."
-          nickname="고양이"
-          date="2023-01-08"
-        />
-        <ReviewItem
-          content="너무 기대돼요."
-          nickname="고양이"
-          date="2023-01-08"
-        />
-        <ReviewItem
-          content="너무 기대돼요."
-          nickname="고양이"
-          date="2023-01-08"
-        />
+        <Input addReview={addReview} />
+        {reviews.map(({ id, content, nickname, date }) => (
+          <ReviewItem
+            id={id}
+            content={content}
+            nickname={nickname}
+            date={getDate(date)}
+          />
+        ))}
       </View>
     </S.Container>
   );
