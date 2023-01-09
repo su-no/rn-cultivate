dd7eca26ae048336a3b23ea6b86d08d6f79000c2
Hyojin <0910.shj@gmail.com>
2023-01-09 11:43:55 +0900

    refactor: 리뷰 read - useQuery로 변경

--- common/api.js
+++ common/api.js
@@ -7,3 +7,15 @@ export const getDetail = async (title) => {
   const path = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000/%20/${encodedTitle}`;
   return fetch(path).then((res) => res.json());
 };
+
+// firebase에서 title과 일치하는 리뷰 받아오는 함수
+const getReviews = async (title) => {
+  const q = query(
+    collection(dbService, 'reviews'),
+    where('title', '==', title),
+  );
+  const reviews = [];
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => reviews.push({ id: doc.id, ...doc.data() }));
+  return reviews;
+};
--- components/ReviewContainer/ReviewContainer.jsx
+++ components/ReviewContainer/ReviewContainer.jsx
@@ -1,38 +1,22 @@
-import { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { collection, query, where, getDocs, addDoc } from 'firebase/firestore';
+import { useQuery } from 'react-query';
+import { collection, addDoc } from 'firebase/firestore';
 import { authService, dbService } from '../../common/firebase';
+import { getReviews } from '../../common/api';
 import { getDate } from '../../common/utils';
 import * as S from './styles';
 import Input from '../Input/Input';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import Loader from '../Loader/Loader';
 
 export default function ReviewContainer({ title }) {
-  // 리뷰 데이터
-  const [reviews, setRivews] = useState([]);
-
   // firebase 현재 유저 정보
   const user = authService.currentUser;
   const nickname = user.displayName;
 
-  // firebase에서 title과 일치하는 리뷰 받아오는 함수
-  const getReviews = async () => {
-    const q = query(
-      collection(dbService, 'reviews'),
-      where('title', '==', title),
-    );
-
-    try {
-      const reviews = [];
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) =>
-        reviews.push({ id: doc.id, ...doc.data() }),
-      );
-      setRivews(reviews);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { isLoading, data: reviewData } = useQuery('reviews', () =>
+    getReviews(title),
+  );
 
   // firebase에 리뷰 추가하는 함수
   const addReview = async (content) => {
@@ -50,19 +34,16 @@ export default function ReviewContainer({ title }) {
     }
   };
 
-  // 컴포넌트 마운트 시 리뷰 데이터 받아오기
-  useEffect(() => {
-    getReviews();
-  }, []);
+  if (isLoading) return <Loader />;
 
   return (
     <S.Container>
       <View>
         <S.Title>후기 & 기대평</S.Title>
         <Input addReview={addReview} />
-        {reviews.map(({ id, content, nickname, date }) => (
+        {reviewData.map(({ id, content, nickname, date }) => (
           <ReviewItem
-            id={id}
+            key={id}
             content={content}
             nickname={nickname}
             date={getDate(date)}
