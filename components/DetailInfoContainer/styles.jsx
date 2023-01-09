6289e3488eeb857a97f5cca265de4d6ac53e1fdb
Hyojin <0910.shj@gmail.com>
2023-01-09 09:48:10 +0900

    Feat: 관심티켓 버튼 추가

--- components/DetailInfoContainer/styles.jsx
+++ components/DetailInfoContainer/styles.jsx
@@ -2,13 +2,13 @@ import styled from '@emotion/native';
 import { BLACK_COLOR } from '../../common/colors';
 import { screenHeight } from '../../common/utils';
 
-export const Container = styled.View`
-  margin: 20px 20px 0px 20px;
-`;
-
 export const Poster = styled.Image`
   width: 100%;
   height: ${`${screenHeight * 0.4}`}px;
+`;
+
+export const Container = styled.View`
+  margin: 20px 20px 0px 20px;
 `;
 
 export const Title = styled.Text`
@@ -17,4 +17,14 @@ export const Title = styled.Text`
   font-weight: bold;
   margin-bottom: 14px;
   color: ${BLACK_COLOR};
+`;
+
+export const TicketContainer = styled.TouchableOpacity`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+`;
+export const Ticket = styled.Image`
+  width: 70px;
+  height: 35px;
 `;
