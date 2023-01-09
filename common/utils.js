30df7877b13076731f3241bb5ce62b86cac3d8e5
Hyojin <0910.shj@gmail.com>
2023-01-09 16:42:02 +0900

    fix: getData 함수 수정
    기존: 현재 시간을 포맷팅
    변경: 인자로 주어진 시간을 포맷팅

--- common/utils.js
+++ common/utils.js
@@ -10,8 +10,8 @@ export const pwRegex =
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 
 // 2022-11-11 13:52 포맷으로 날짜를 받아오는 함수
-export const getDate = () => {
-  const date = new Date();
+export const getDate = (d) => {
+  const date = new Date(d);
   const year = String(date.getFullYear()).padStart(4, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
