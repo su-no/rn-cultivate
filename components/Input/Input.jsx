0c9257c126e2a4700293b3c9a48647e9ced55de8
Hyojin <0910.shj@gmail.com>
2023-01-09 22:12:47 +0900

    feat: 비로그인 상태일 때 로그인 페이지로 이동
    비로그인 상태일 때 후기&기대평 클릭 시 로그인 페이지로 이동

--- components/Input/Input.jsx
+++ components/Input/Input.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { authService } from '../../common/firebase';
 import { GRAY_COLOR } from '../../common/colors';
 import * as S from './styles';
 
 export default function Input({ addReview }) {
-  const [content, setContent] = useState('');
-  const user = authService.currentUser;
+  const { push } = useNavigation();
 
+  const [content, setContent] = useState('');
+
+  // 리뷰 등록
   const onSubmit = async () => {
     // 유효성 검사
     if (content.trim() === '') {
@@ -15,15 +18,34 @@ export default function Input({ addReview }) {
       setContent('');
       return;
     }
+    // 등록
     await addReview(content);
+    // 초기화
     setContent('');
   };
 
+  const user = authService.currentUser;
+
   return (
     <>
+      {/* 로그인한 유저가 없으면 작성 불가능 */}
       {!user ? (
         <S.Input
-          onPressOut={() => console.log('로그인 페이지로 이동')}
+          onPressOut={() =>
+            Alert.alert('로그인', '로그인 페이지로 이동하시겠습니까?', [
+              {
+                text: '취소',
+                style: 'cancel',
+              },
+              {
+                text: '확인',
+                style: 'default',
+                onPress: () => {
+                  push('Stack', { screen: 'Login' });
+                },
+              },
+            ])
+          }
           placeholder="로그인이 필요한 서비스입니다."
           placeholderTextColor={GRAY_COLOR}
           backgroundColor="#e8e8e8"
@@ -31,10 +53,10 @@ export default function Input({ addReview }) {
         />
       ) : (
         <S.Input
+          placeholder="내용을 입력하세요."
           value={content}
           onChangeText={setContent}
           onSubmitEditing={onSubmit}
-          placeholder="내용을 입력하세요."
           maxLength={50}
         />
       )}
