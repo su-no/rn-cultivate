46a73b94b8da7c67af5742b579e57d0cced55cdc
innasz <mini5167@naver.com>
2023-01-10 22:39:44 +0900

    feat. Main ScrollView

--- common/api.js
+++ common/api.js
@@ -7,3 +7,10 @@ export const getDetail = async (title) => {
   const path = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1000/%20/${encodedTitle}`;
   return fetch(path).then((res) => res.json());
 };
+
+export const getData = async () => {
+  const path = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/10`;
+  return fetch(path)
+    .then((res) => res.json())
+    .then((data) => data.culturalEventInfo.row);
+};
--- screens/Main.jsx
+++ screens/Main.jsx
@@ -1,53 +1,113 @@
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import { screenHeight, screenWidth } from '../common/utils';
+import { screenHeight } from '../common/utils';
 import styled from '@emotion/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Swiper from 'react-native-swiper';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { getData } from '../common/api';
+import { useQuery } from 'react-query';
 
 export default function Main({ navigation: { navigate } }) {
+  const { data, isLoading } = useQuery({
+    queryKey: 'data',
+    queryFn: () => getData(),
+  });
+  if (isLoading) {
+    return (
+      <View>
+        <Text>로딩중입니다</Text>
+      </View>
+    );
+  }
+
+  const [show, setShow] = useState('');
+
+  const [onStage, setOnStage] = useState([]);
+  const [upComings, setUpComings] = useState([]);
+
   return (
-    <ScrollView>
-      <BannerContainer>
-        <Image
-          source={require('../assets/banner.png')}
-          style={{ width: screenWidth, height: screenHeight / 4 }}
-        />
-      </BannerContainer>
+    <ScrollView style={{ flex: 1 }}>
+      <Swiper height="100%" showsPagination={false} autoplay loop>
+        <SwiperChildView>
+          <BackgroundImg
+            style={StyleSheet.absoluteFill}
+            source={{ uri: data[0]?.MAIN_IMG }}
+          />
+          <LinearGradient
+            style={{ position: 'absolute', top: 0, left: 0 }}
+            colors={['transparent', 'red']}
+          />
+        </SwiperChildView>
+
+        <SwiperChildView>
+          <BackgroundImg
+            style={StyleSheet.absoluteFill}
+            source={require('../assets/color.jpg')}
+          />
+          <LinearGradient
+            style={{ position: 'absolute', top: 0, left: 0 }}
+            colors={['transparent', 'black']}
+          />
+        </SwiperChildView>
+        <SwiperChildView>
+          <BackgroundImg
+            style={StyleSheet.absoluteFill}
+            source={require('../assets/cats.jpg')}
+          />
+          <LinearGradient
+            style={{ position: 'absolute', top: 0, left: 0 }}
+            colors={['transparent', 'black']}
+          />
+        </SwiperChildView>
+      </Swiper>
       <MainAllContainer>
         <OnStageContainer>
           <TitleText>
             <Text style={{ fontSize: 30 }}>On Stage</Text>
           </TitleText>
-          <OnStageBox>
-            <Image
-              source={require('../assets/pansori.png')}
-              style={{ width: 100, height: 200 }}
-            />
-            <Image
-              source={require('../assets/younha.jpg')}
-              style={{ width: 100, height: 200 }}
-            />
-            <Image
-              source={require('../assets/cats.jpg')}
-              style={{ width: 100, height: 200 }}
-            />
-          </OnStageBox>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 20 }}
+          >
+            <OnStageBox>
+              <TouchableOpacity>
+                <Image
+                  resizeMode="cover"
+                  source={{ uri: data[0]?.MAIN_IMG }}
+                  style={{ width: 100, height: 200 }}
+                />
+                {/* <Text>{data[0]?.TITLE}</Text> */}
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image
+                  resizeMode="cover"
+                  source={{ uri: data[1]?.MAIN_IMG }}
+                  style={{ width: 100, height: 200 }}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image
+                  resizeMode="cover"
+                  source={{ uri: data[2]?.MAIN_IMG }}
+                  style={{ width: 100, height: 200 }}
+                />
+              </TouchableOpacity>
+            </OnStageBox>
+          </ScrollView>
         </OnStageContainer>
         <UpcomingContainer>
           <TitleText>
             <Text style={{ fontSize: 30 }}>Upcoming</Text>
           </TitleText>
           <UpcomingBox>
-            <Image
-              source={require('../assets/pansori.png')}
-              style={{ width: 100, height: 200 }}
-            />
-            <Image
-              source={require('../assets/younha.jpg')}
-              style={{ width: 100, height: 200 }}
-            />
-            <Image
-              source={require('../assets/cats.jpg')}
-              style={{ width: 100, height: 200 }}
-            />
+            <TouchableOpacity>
+              <Image
+                source={require('../assets/cats.jpg')}
+                style={{ width: 100, height: 200 }}
+              />
+            </TouchableOpacity>
           </UpcomingBox>
         </UpcomingContainer>
         {/* 상세페이지 임시 코드 */}
@@ -62,23 +122,37 @@ export default function Main({ navigation: { navigate } }) {
   );
 }
 
-const BannerContainer = styled.View``;
+const SwiperChildView = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  height: ${screenHeight / 3 + 'px'};
+  background-color: green;
+`;
+
+const BackgroundImg = styled.Image`
+  height: 100%;
+  width: 100%;
+`;
+
 const MainAllContainer = styled.View`
   padding: 20px;
 `;
 const OnStageContainer = styled.View``;
+
 const OnStageBox = styled.View`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  width: 350px;
 `;
+
 const UpcomingContainer = styled.View``;
+
 const UpcomingBox = styled.View`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 `;
-
 const TitleText = styled.Text`
   font-weight: 200;
   margin-top: 20px;
